54209579294e7c1ce974bfdd723b6c3641767268
Administrador <Administrador@Golden-PC>
2022-01-06 19:08:33 -0500

    Cambio 2

--- JavaApplication4/Backup/src/main/java/com/mycompany/backup/Carros.java
+++ JavaApplication4/Backup/src/main/java/com/mycompany/backup/Carros.java
@@ -5,6 +5,8 @@
  */
 package com.mycompany.backup;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Administrador
@@ -15,7 +17,30 @@ public class Carros {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //instancia mi clase
+        Atributos objeto =new Atributos();
+        //Nombre clase +objeto(variable)
+        String propietario = JOptionPane.showInputDialog("Introduce el Propietario ");
+          
+        String valor = JOptionPane.showInputDialog("Introduce valor comercial");
+        int valorcomercial = Integer.parseInt(valor);
+        String color = JOptionPane.showInputDialog("Introduce el color ");
+        String placa = JOptionPane.showInputDialog("Introduce la Placa ");
+        String matriculado = JOptionPane.showInputDialog("Introduce Si esta matriculado ");
+        String anio = JOptionPane.showInputDialog("Introduce Año de Matriculacion ");
+        int aniofabricacion = Integer.parseInt(anio);
+       
+        objeto.setPropietario(propietario);
+        objeto.setValorcomercial(valorcomercial);
+        objeto.setColor(color);
+        objeto.setMatriculado(matriculado);
+        objeto.setAniofabricacion(aniofabricacion);
+      
+
+        JOptionPane.showMessageDialog(null, objeto.datos() + "\n"
+                + objeto.calcularAnio() + "\n" );
+// click derecho = format ( restroctura el codigo)
+    
     }
     
 }
